6c53fbc311ebe996e5cb6e6a2b31a1b8c6e32dd8
Yonathan Uchoa S. Kaveski <yonathan.uchoa@gmail.com>
2023-05-18 16:59:40 -0400

    feat: create swagger conf

---
+++ src/configs/swagger-conf.js
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv';
+import swaggerJsDoc from 'swagger-jsdoc';
+
+dotenv.config({path: `env/.env`});
+
+const PORT = process.env.SERVER_PORT || 3000;
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Movie DB API",
+            version: "0.1.0",
+            description: "TMDB routing API"
+        },
+        servers: [
+            {
+                url: `http://localhost:${PORT}`
+            }
+        ]        
+    },
+    apis: ["**/handlers/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+
+export default specs;
+
